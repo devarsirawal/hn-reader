19939c2d48376efa6a7029c20056f345f9f1f7cb
Devarsi Rawal <devarsirawal@gmail.com>
2020-08-02 18:21:23 -0500

    Remove infinite scroll

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useState, memos } from 'react';
-import { getStoryIds } from '../services/hackerNewsApi';
+import { getNewStoryIds, getBestStoryIds } from '../services/hackerNewsApi';
 import { Story } from '../components/Story';
 import { GlobalStyle, StoriesContainerWrapper } from '../styles/StoriesContainerStyles';
-import { useInfiniteScroll } from '../hooks/useInfiniteScroll';
 
-export const StoriesContainer = () => {
-    const { count } = useInfiniteScroll();
+export const StoriesContainer = ({ storiesCall }) => {
     const [storyIds, setStoryIds] = useState([]);
 
     //TODO: Add a try-catch
     useEffect(() => {
-      getStoryIds().then(data => setStoryIds(data));
+      storiesCall().then(data => setStoryIds(data));
     }, []);
-  
-  
+
     return(
         <div>
             <GlobalStyle />
             <StoriesContainerWrapper data-testid="stories-container">
                 <h1>Hacker News Stories</h1> 
-                {storyIds.slice(0,count).map(storyId => (<Story key={storyId} storyId={storyId} />))}
+                {storyIds.map(storyId => (<Story key={storyId} storyId={storyId} />))}
             </StoriesContainerWrapper>
         </div>
     ); 
